3e7c2753a6aa1e813bd7937bd579fc2d6cbacd8a
Devheun <dudwo5600@korea.ac.kr>
2024-09-20 00:28:42 +0900

    refactor:: 시간 UTC -> 한국시간으로 변경

--- src/main/java/me/leesiheun/springbootdeveloper2/SpringBootDeveloper2Application.java
+++ src/main/java/me/leesiheun/springbootdeveloper2/SpringBootDeveloper2Application.java
@@ -1,13 +1,21 @@
 package me.leesiheun.springbootdeveloper2;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class SpringBootDeveloper2Application {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDeveloper2Application.class, args);
+    }
+
+    @PostConstruct
+    public void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 }
